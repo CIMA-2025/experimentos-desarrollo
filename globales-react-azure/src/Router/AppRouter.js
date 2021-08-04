505f903e89fac3ab122f4bf697a98b9f052f2a7d
Miguel Manzano Rodriguez <70585098+ManzanoRdz94@users.noreply.github.com>
2021-08-04 15:54:09 -0500

    Add files via upload

---
+++ globales-react-azure/src/Router/AppRouter.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+import React from "react";
+
+
+import Block from "../Block";
+import Intro from "../Intro";
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Intro} />
+                    <Route exact path="/block" component={Block} />
+                </Switch>
+            </Router>
+            <MessengerCustomerChat pageId="109132038102012" appId="320817016358987" />
+        </>
+    );
+}
